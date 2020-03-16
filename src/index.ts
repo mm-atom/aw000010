4519bab0ce4f4c1cca6327b41aa0dc16cd0ebadf
taoqf <tao_qf@163.com>
2020-03-16 09:16:15 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-
-export default async function aw10(param1: string, param2: string) {
+/**
+ * 设置全局存储
+ * @param key 键
+ * @param value 值
+ */
+export default function set(key: string, value: unknown) {
+	sessionStorage.setItem(key, JSON.stringify(value));
 }
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,19 @@
 import { expect } from 'chai';
 import t from '../dist/index';
 
-describe('xxx', () => {
-	it('xxx', () => {
-		t();
-		expect(1).not.eq(2);
+function get(key) {
+	const str = sessionStorage.getItem(key);
+	if (str) {
+		return JSON.parse(str);
+	} else {
+		return null;
+	}
+}
+
+describe('global set', async () => {
+	it('set global value', async () => {
+		t('key', '123456wk');
+		const value = get('key')
+		expect('123456wk').eq(value)
 	});
 });
